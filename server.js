24175124036c01078936007075baf701cd0e7a16
sanskargangwar <145826905+sanskargangwar@users.noreply.github.com>
2026-06-16 13:47:45 +0530

    Bind server to all network interfaces

--- server.js
+++ server.js
@@ -19,6 +19,6 @@ app.use("/api", require("./routes/userroutes"));
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
